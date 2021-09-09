cda10aa544565faad9174ad6e312457449712a34
dipak022 <dipakd3ebnath4002@gmail.com>
2021-09-09 06:00:59 +0600

    jwt token store computer localStorage so,make a helper class Token create an write down method

---
+++ resources/js/helpers/Token.js
@@ -0,0 +1,30 @@
+class Token {
+
+
+    isValid(token){
+        const paylpad = this.paylpad(token);
+
+        if(paylpad){
+
+            return paylpad.iss == "http://127.0.0.1:8000/" || "http://127.0.0.1:8000/register" ? true : false;
+
+        }
+
+        return false;
+    }
+
+    paylpad(token){
+        const paylpad = token.split('.')[1];
+        return this.decode(paylpad);
+    }
+
+
+    decode(paylpad){
+
+        return JSON.parse(atob(paylpad));
+
+    }
+
+
+}
+export default Token = new Token();
